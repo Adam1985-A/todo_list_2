740320a07e23e3c67249ec9bd5400b7672cb74b6
Raheem Adam <raheemadam85@gmail.com>
2026-01-18 07:44:21 +0100

    initial commit

--- src/database/data-source.js
+++ src/database/data-source.js
@@ -1,7 +1,8 @@
-import "reflect-metadata";
-import { DataSource } from "typeorm";
 import dotenv from "dotenv";
 dotenv.config();
+console.log("DB URL exists:", !!process.env.DATABASE_URL);
+import "reflect-metadata";
+import { DataSource } from "typeorm";
 import TodoEntity from "../entity/todo.entity.js";
 import UserEntity from "../entity/user.entity.js";
 
@@ -11,11 +12,6 @@ const AppDataSource = new DataSource({
   ssl: process.env.NODE_ENV === "production"
    ? { rejectUnauthorized: false } 
    : false,
-  host: "localhost",
-  port: [5432, 10],
-  username: "postgres",
-  password: "saidat1985", 
-  database: "todo_list_2",  
   synchronize: true,
   logging: false,
   entities: [TodoEntity, UserEntity],
